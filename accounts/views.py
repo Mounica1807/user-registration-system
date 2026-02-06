b5ff9152bdefcbe5d6e28f621e404d4d9bb9953f
Mounica1807 <mounica1807@gmail.com>
2026-02-06 21:37:43 +0530

    Fix import error: remove missing CustomTokenObtainPairSerializer

--- accounts/views.py
+++ accounts/views.py
@@ -27,4 +27,5 @@ class ProfileView(generics.RetrieveAPIView):
         return self.request.user
     
 class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
     permission_classes = [AllowAny]
