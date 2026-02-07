7ca0e2213879decb2b26b5eab154de98e2e2a144
Mounica1807 <mounica1807@gmail.com>
2026-02-07 19:20:05 +0530

    Add create_superuser function definition

--- core/urls.py
+++ core/urls.py
@@ -4,23 +4,23 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 
-# Temporary endpoint to create/reset superuser (REMOVE AFTER USE!)
+# Temporary superuser creation/reset (remove after use!)
 def create_superuser(request):
     User = get_user_model()
-    email = 'mouni@gmail.com'  # ← your email
+    email = 'mouni@gmail.com'  # your email
     if User.objects.filter(email=email).exists():
         return HttpResponse("Superuser already exists. Go to /admin/ to login.")
     else:
         User.objects.create_superuser(
             email=email,
             username='mouni',
-            password='mouni123456'  # ← CHANGE THIS to a password YOU WILL REMEMBER
+            password='mouni1807'  # CHANGE THIS PASSWORD NOW!
         )
-        return HttpResponse("Superuser created! Now login at /admin/")
+        return HttpResponse("Superuser created! Login at /admin/")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('create-superuser/', create_superuser),  # temporary line - remove later
+    path('create-superuser/', create_superuser),  # temporary line
 ]
