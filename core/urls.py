a23a029723fb4ed48396d8a88d4e3f38d55b4c08
Mounica1807 <mounica1807@gmail.com>
2026-02-07 19:26:36 +0530

    Add create_superuser function definition

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, ProfileView
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 
-# TEMPORARY FUNCTION - only to reset/create superuser (REMOVE AFTER USE!)
+# TEMPORARY FUNCTION - only for creating/resetting superuser (REMOVE AFTER USE!)
 def create_superuser(request):
     User = get_user_model()
     email = 'mouni@gmail.com'  # your email
@@ -14,13 +14,14 @@ def create_superuser(request):
         User.objects.create_superuser(
             email=email,
             username='mouni',
-            password='mouni1807'  # CHANGE THIS TO A PASSWORD YOU WILL REMEMBER
+            password='mouni1807'  # CHANGE THIS TO A SECURE PASSWORD YOU WILL REMEMBER
         )
         return HttpResponse("Superuser created! Now login at /admin/")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('create-superuser/', create_superuser),  # temporary line
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('create-superuser/', create_superuser),  # temporary line - delete after use
 ]
