8538de6a30cb6037da7be7dae41957398e2e1e3f
Mounica1807 <mounica1807@gmail.com>
2026-02-07 08:50:18 +0530

    Add missing import for TokenObtainPairView in urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import RegisterView, CustomTokenObtainPairView, ProfileView
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
