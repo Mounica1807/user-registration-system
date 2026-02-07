972787193ce94a9d61d0b6834bf40a688ba6b46f
Mounica1807 <mounica1807@gmail.com>
2026-02-07 19:47:01 +0530

    Remove broken create-superuser path (fix NameError)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import RegisterView, CustomTokenObtainPairView, ProfileView
-from rest_framework_simplejwt.views import TokenRefreshView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('create-superuser/', create_superuser),
 ]
