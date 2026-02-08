877a0b30d480292ad77d188d8963f56e04e193d9
Mounica1807 <mounica1807@gmail.com>
2026-02-08 20:22:39 +0530

    Add create_superuser function in accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, ProfileView
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+# TEMPORARY FUNCTION - only for creating/resetting superuser (REMOVE AFTER USE!)
+def create_superuser(request):
+    User = get_user_model()
+    email = 'mouni@gmail.com'  # your email
+    if User.objects.filter(email=email).exists():
+        return HttpResponse("Superuser already exists. Go to /admin/ to login.")
+    else:
+        User.objects.create_superuser(
+            email=email,
+            username='mouni',
+            password='mouni1807'  # CHANGE THIS TO A SECURE PASSWORD YOU WILL REMEMBER
+        )
+        return HttpResponse("Superuser created! Now login at /admin/")
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('create-superuser/', create_superuser),  # temporary line - delete after use
 ]
